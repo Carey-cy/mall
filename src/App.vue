83b4a4fba5f7a9e40a0237ab1b7045a69538d276
cy <changanazai@163.com>
2023-03-10 15:50:38 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
-    
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
+<script>
+
+import MainTabBar from 'components/content/mainTabbar/MainTabBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainTabBar,
+  }
+ 
+}
+</script>
+
 <style>
-  @import 'assets/css/base.css'
+  @import url('assets/css/base.css')
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: 'Home',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

c4e94e45ec34db0458a8e156544584b8333fcd87
cy <changanazai@163.com>
2023-03-28 21:11:46 +0800

    详情页detail获取数据&轮播图&title写入

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="detail">
     <detail-nav></detail-nav>
+    <detail-swiper :topImg="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
 import DetailNav from 'views/detail/childComps/DetailNav.vue'
+import DetailSwiper from 'views/detail/childComps/DetailSwiper.vue'
+import DetailBaseInfo from 'views/detail/childComps/DetailBaseInfo.vue'
 
+import { getDetails, Goods } from 'network/detail.js'
 export default {
   name: 'Detail',
   data () {
     return {
-      id: null
+      id: null,
+      topImages: [],
+      goods: null
     }
   },
   components: {
-    DetailNav
+    DetailNav,
+    DetailSwiper,
+    DetailBaseInfo
   },
   created () {
     this.id = this.$route.params.iid
+    // 请求详情页对应数据
+    getDetails(this.id).then(res => {
+      console.log(res)
+      const data = res.result
+      // 获取轮播图数据
+      this.topImages = data.itemInfo.topImages
+      // 获取商品信息数据
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+    })
   }
 }
 </script>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <mt-swipe>
+    <mt-swipe-item  :auto="3000" v-for="(item,index) in topImg" :key="index">
+        <img :src="item" alt="">
+     </mt-swipe-item>
+  </mt-swipe>
+</template>
+
+<script>
+export default {
+  name: 'DetailSwiper',
+  props: {
+    topImg: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 100%;
+  }
+  .mint-swipe {
+    height: 300px;
+    overflow: hidden;
+  }
+</style>

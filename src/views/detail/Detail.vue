f50c640862236cd5b361017126c55ba809e29fc3
cy <changanazai@163.com>
2023-03-29 01:07:06 +0800

    detail店家以及商品信息展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="detail">
-    <detail-nav></detail-nav>
-    <detail-swiper :topImg="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"/>
+    <detail-nav class="detail-nav"></detail-nav>
+    <scroll class="content">
+      <detail-swiper :topImg="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+    </scroll>
   </div>
 </template>
 
 <script>
-import DetailNav from 'views/detail/childComps/DetailNav.vue'
-import DetailSwiper from 'views/detail/childComps/DetailSwiper.vue'
-import DetailBaseInfo from 'views/detail/childComps/DetailBaseInfo.vue'
+import DetailNav from './childComps/DetailNav.vue'
+import DetailSwiper from './childComps/DetailSwiper.vue'
+import DetailBaseInfo from './childComps/DetailBaseInfo.vue'
+import DetailShopInfo from './childComps/DetailShopInfo.vue'
 
-import { getDetails, Goods } from 'network/detail.js'
+import { getDetails, Goods, Shop } from 'network/detail.js'
+
+import Scroll from 'components/common/scroll/Scroll.vue'
+
 export default {
   name: 'Detail',
   data () {
     return {
       id: null,
       topImages: [],
-      goods: null
+      goods: {},
+      shop: {}
     }
   },
   components: {
     DetailNav,
     DetailSwiper,
-    DetailBaseInfo
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll
   },
   created () {
     this.id = this.$route.params.iid
@@ -36,11 +46,27 @@ export default {
       this.topImages = data.itemInfo.topImages
       // 获取商品信息数据
       this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      // 获取店铺信息
+      this.shop = new Shop(data.shopInfo)
     })
   }
 }
 </script>
 
 <style scoped>
-
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .content {
+    height: calc(100% - 44px);
+    overflow: hidden;
+  }
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
 </style>
